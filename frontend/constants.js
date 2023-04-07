39f2d82035a1ea3f0e7eb9ddc9f3fa7645eaa05f
Hita Juneja <hitajuneja@gmail.com>
2023-04-07 14:15:32 +0530

    reset

--- frontend/constants.js
+++
@@ -1,7 +0,0 @@
-export const BASE_URL = 'http://localhost:3004'; 
-// paste the  url api gateway is running on
-
-export const FALLBACK_IMAGE =
-  'https://bitsofco.de/content/images/2018/12/broken-1.png';
-export const GENERIC_ERROR = 'An Error has occurred';
-export const NAIRA_SYMBOL = '₦';

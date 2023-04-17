6974aca324d491d1577a9198805d589679a0877a
sailaja2003 <sailajapolicharla2003@gmail.com>
2023-04-17 16:43:36 +0530

    product-service

---
+++ product-service/src/main/java/com/cc/productservice/service/ProductService.java
@@ -0,0 +1,47 @@
+package com.cc.productservice.service;
+
+import com.cc.productservice.dto.ProductRequest;
+import com.cc.productservice.dto.ProductResponse;
+import com.cc.productservice.model.Product;
+import com.cc.productservice.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public void createProduct(ProductRequest productRequest) {
+        Product product = Product.builder()
+                .name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
+
+        productRepository.save(product);
+        log.info("Product {} is saved", product.getId());
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+
+        return products.stream().map(this::mapToProductResponse).toList();
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
+    }
+
+    
+}

9fa1ada2277ff0676f7b84f1d59372805001e9b9
hita03 <hitajuneja@gmail.com>
2023-04-23 01:45:27 +0530

    build trigger

--- product-service/src/main/java/com/programmingtechie/productservice/service/ProductService.java
+++ product-service/src/main/java/com/programmingtechie/productservice/service/ProductService.java
@@ -34,6 +34,7 @@ public class ProductService {
         return products.stream().map(this::mapToProductResponse).toList();
     }
 
+    // build trigger
     private ProductResponse mapToProductResponse(Product product) {
         return ProductResponse.builder()
                 .id(product.getId())
